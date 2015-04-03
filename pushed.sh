e04331c966c3ccc30b750888fb1d3a046d98527b
Joey Hess <joeyh@joeyh.name>
2015-04-03 01:51:38 -0400

    fix cd

--- pushed.sh
+++ pushed.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Run as root by pushme.cgi.
 base="$(dirname $0)"
-(cd "$base/pubkeys" && git pull)
+cd "$base/pubkeys"
+git pull
 for SHARD in SHARD*; do
 	"$base/update-authorized_keys" "$SHARD"
 done
