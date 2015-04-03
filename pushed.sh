4e1486f8f767c9ef1a53bd2e720c18a7f1405d6a
Joey Hess <joeyh@joeyh.name>
2015-04-03 01:49:24 -0400

    typo

--- pushed.sh
+++ pushed.sh
@@ -2,6 +2,6 @@
 # Run as root by pushme.cgi.
 base="$(dirname $0)"
 (cd "$base/pubkeys" && git pull)
-for "$SHARD" in SHARD*; do
+for $SHARD in SHARD*; do
 	"$base/update-authorized_keys" "$SHARD"
 done
