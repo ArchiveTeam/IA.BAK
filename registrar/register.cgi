617d9b0aafc80042ea5f0f56fd11cd0805741225
Joey Hess <joeyh@joeyh.name>
2015-04-21 23:51:53 -0400

    QUERY_STRING proopigation across sudo

--- registrar/register.cgi
+++ registrar/register.cgi
@@ -2,4 +2,4 @@
 set -e
 echo "Content-Type: text/plain"
 echo ""
-sudo -u registrar /home/registrar/IA.BAK/registrar/register.pl
+sudo -u registrar /home/registrar/IA.BAK/registrar/register.pl "$QUERY_STRING"
--- registrar/register.pl
+++ registrar/register.pl
@@ -2,26 +2,37 @@
 use warnings;
 use strict;
 use CGI;
+
+$ENV{QUERY_STRING}=shift @ARGV;
+
 my $q = CGI->new;
 my $email = $q->param("email");
 my $shard = $q->param("shard");
 my $pubkey = $q->param("pubkey");
 if ($shard !~ /^SHARD\d+$/) {
-	die "bad SHARD\n";
+	oops("bad SHARD");
 }
 my ($sanitized_pubkey)=$pubkey=~/^(ssh-rsa [^\s]+)/;
 my ($sanitized_email)=$email=~/^([^\s]+)/;
 if ($sanitized_pubkey == "" || $sanitized_email == "") {
-	die "bad inputs";
+	oops("bad inputs");
 }
 
-chdir("/home/registrar/users/") || die "chdir: $!";
+chdir("/home/registrar/users/") || oops("chdir: $!");
 if (! -d $shard) {
-	mkdir("$shard") || die "mkdir: $!";
+	mkdir("$shard") || oops("mkdir: $!");
 }
-open (OUT, ">>$shard/pubkeys") || die "open: $!";
+open (OUT, ">>$shard/pubkeys") || oops("open: $!");
 print OUT "$sanitized_pubkey $sanitized_email";
 close OUT;
 system("git add $shard/pubkeys; git commit -m registration; git push");
 print "** REGISTRATION SUCCEEDED **";
 exit 0;
+
+sub oops {
+	my $msg=shift;
+	print "REGISRATION FAILED: $msg\n";
+	print "";
+	print "Please contact the #internetarchive.bak IRC channel on irc.efnet.org for support!";
+	exit 1;
+}
