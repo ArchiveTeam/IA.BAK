2b8c559650e0e09258ae892b4c96bcb18a61242a
Joey Hess <joeyh@joeyh.name>
2015-04-21 23:38:57 -0400

    sanitize inputs

--- registrar/register.pl
+++ registrar/register.pl
@@ -9,13 +9,18 @@ my $pubkey = $q->param("pubkey");
 if ($shard !~ /^SHARD\d+$/) {
 	die "bad SHARD\n";
 }
+my ($sanitized_pubkey)=$pubkey=~/^(ssh-rsa [^\s]+)/;
+my ($sanitized_email)=$email=~/^([^\s]+)/;
+if ($sanitized_pubkey == "" || $sanitized_email == "") {
+	die "bad inputs";
+}
 
 chdir("/home/registrar/users/") || die "chdir: $!";
 if (! -d $shard) {
 	mkdir("$shard") || die "mkdir: $!";
 }
 open (OUT, ">>$shard/pubkeys") || die "open: $!";
-print OUT "$pubkey $email";
+print OUT "$sanitized_pubkey $sanitized_email";
 close OUT;
 system("git add $shard/pubkeys; git commit -m registration; git push");
 print "** REGISTRATION SUCCEEDED **";
