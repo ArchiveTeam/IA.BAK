bd69c67902e314c0e5dc9320aa906dcafec48e38
Joey Hess <joeyh@joeyh.name>
2015-04-21 23:31:14 -0400

    WIP cgi to add keys

--- registrar/register.pl
+++ registrar/register.pl
@@ -1,3 +1,22 @@
 #!/usr/bin/perl
-print "TOOD\n";
-exit 1
+use warnings;
+use strict;
+use CGI;
+my $q = CGI->new;
+my $email = $q->param("email");
+my $shard = $q->param("shard");
+my $pubkey = $q->param("pubkey");
+if ($shard !~ /^SHARD\d+$/) {
+	die "bad SHARD\n";
+}
+
+chdir("/home/registrar/users/") || die "chdir: $!";
+if (! -d $shard) {
+	mkdir("$shard") || die "mkdir: $!";
+}
+open (OUT, ">>$shard/pubkeys") || die "open: $!";
+print OUT "$pubkey $email";
+close OUT;
+system("git add $shard/pubkeys; git commit -m registration; git push");
+print "** REGISTRATION SUCCEEDED **";
+exit 0;
