2d9a171ac24c53542d44158b81236b90b0de0b3b
Joey Hess <joeyh@joeyh.name>
2015-04-22 01:58:44 -0400

    use CGI.pm to generate urls

---
+++ register-helper.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use CGI;
+
+my ($shard, $uuid, $email, $pubkey) = @ARGV;
+@ARGV=();
+
+my $q=CGI->new;
+$q->param("shard", $shard);
+$q->param("uuid", $uuid);
+$q->param("email", $email);
+$q->param("pubkey", $pubkey);
+my $url=$q->self_url;
+$url=~s!^http://localhost!http://iabak.archiveteam.org/cgi-bin/register.cgi!;
+print "$url\n";
