edba57a030803a30376b364db320ce1822dbadf0
Joey Hess <joeyh@joeyh.name>
2015-05-21 23:19:22 -0400

    add links to individual shard pages

--- web/graph-gen.sh
+++ web/graph-gen.sh
@@ -95,7 +95,21 @@ cat $SHARD.clientconnsperhour | sed -e 's/^[ \t]*//' | awk '{print $2, $3, $4, "
    done
    LEADERBOARDURL="http://iabak.archiveteam.org/stats/$SHARD.leaderboard"
 
-   cat html/graph.template.tail | sed "s/SHARDNAME/${SHARDNAME}/g" | sed "s!COLLDESC!${COLLDESC}!g" | sed "s!COLLECTIONS!${COLLECTIONS}!g" | perl -pe "s!CONNECTIONSGRAPHURL!${CONNECTIONSGRAPHURL}!g" | perl -pe "s!PROGRESS_GRAPHURL!${PROGRESS_GRAPHURL}!g" | sed "s/SIZE/${SIZE}/g" | sed "s/IA1/${IA1}/g" | sed "s/IA2/${IA2}/g" | sed "s/IA3/${IA3}/g" | sed "s/IA4/${IA4}/g" | sed "s/TIME/${CHRONOS}/g" | sed "s/CLICOUNT/${CLICOUNT}/g" | sed "s/CLAMBAKE/${COUNTRYS}/g" | sed "s!LEADERBOARDURL!${LEADERBOARDURL}!" | sed "s/LEADERBOARD/${LEADERBOARD}/" >> "$HTMLTMP"
+   if [ "$SHARD" = ALL ]; then
+	ACTIVESHARDS="Active: "
+	for s in $(cat ALL.shardlist.active); do
+		ACTIVESHARDS="$ACTIVESHARDS <a href="$s.html">$s</a>"
+	done
+	MAINTSHARDS="Maint: "
+	for s in $(tail -20 ALL.shardlist.maint); do
+		MAINTSHARDS="$MAINTSHARDS <a href="$s.html">$s</a>"
+	done
+   else
+	ACTIVESHARDS=""
+	MAINTSHARDS=""
+   fi
+
+   cat html/graph.template.tail | sed "s/SHARDNAME/${SHARDNAME}/g" | sed "s!COLLDESC!${COLLDESC}!g" | sed "s!COLLECTIONS!${COLLECTIONS}!g" | perl -pe "s!CONNECTIONSGRAPHURL!${CONNECTIONSGRAPHURL}!g" | perl -pe "s!PROGRESS_GRAPHURL!${PROGRESS_GRAPHURL}!g" | sed "s/SIZE/${SIZE}/g" | sed "s/IA1/${IA1}/g" | sed "s/IA2/${IA2}/g" | sed "s/IA3/${IA3}/g" | sed "s/IA4/${IA4}/g" | sed "s/TIME/${CHRONOS}/g" | sed "s/CLICOUNT/${CLICOUNT}/g" | sed "s/CLAMBAKE/${COUNTRYS}/g" | sed "s!LEADERBOARDURL!${LEADERBOARDURL}!" | sed "s/LEADERBOARD/${LEADERBOARD}/" | sed "s!MAINTSHARDS!${MAINTSHARDS}!" | sed "s!ACTIVESHARDS!${ACTIVESHARDS}!" >> "$HTMLTMP"
 
 fi
 
